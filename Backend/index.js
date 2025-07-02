b3ecaa9ecda4e6acc3930220406d50737c54b8a8
navaj bloch <navajbloch420@gmail.com>
2025-07-02 17:22:26 +0530

    remove some npm packgage

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
 import router from './routes/reportRoutes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-connectDB();
+ 
 
 
 //routes 
